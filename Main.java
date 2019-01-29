589852bca3c610f914576bde9c6edb63031e41d5
Abigail González Hidalgo <abigailgonzalezh@gmail.com>
2019-01-28 23:32:14 -0600

    coche y perro

--- Main.java
+++ Main.java
@@ -54,6 +54,61 @@ public class Main extends JPanel{
         a.setColor(new Color(255,255,0));
         a.drawOval(308,296,10,10);
         a.fillOval(308,296,10,10);
+
+        //Car
+        int[] carstructurex = {689,681,687,698,708,716,785,830,881,953,981,988,982,974,970,961,939,912,875,835,812,794,777,764,751,729,713,699,691};
+        int[] carstructurey = {348,356,367,367,367,367,367,367,367,367,367,359,349,341,322,297,267,246,234,232,237,244,260,276,290,293,300,315,332};
+        a.setColor(new Color(255,204,0));
+        a.drawPolygon(carstructurex, carstructurey, 29);
+        a.fillPolygon(carstructurex, carstructurey, 29);
+
+        //Left wheel
+        a.setColor(new Color(0,0,0));
+        a.drawOval(729, 351, 60, 60);
+        a.fillOval(729, 351, 60, 60);
+        a.setColor(new Color(204,204,179));
+        a.drawOval(745, 365, 30, 30);
+        a.fillOval(745, 365, 30, 30);
+        //Right wheel
+        a.setColor(new Color(0,0,0));
+        a.drawOval(897, 351, 60, 60);
+        a.fillOval(897, 351, 60, 60);
+        a.setColor(new Color(204,204,179));
+        a.drawOval(910, 365, 30, 30);
+        a.fillOval(910, 365, 30, 30);
+
+        //Left window
+        int [] x1 = {836,836,806,791,780,772};
+        int [] y1 = {291,244,251,263,276,291};
+        a.setColor(new Color(230,255,255));
+        a.drawPolygon(x1,y1,6);
+        a.fillPolygon(x1,y1,6);
+        //Right window
+        int [] x2 = {851,898,944,933,918,888,852};
+        int [] y2 = {291,291,291,277,265,249,243};
+        a.setColor(new Color(230,255,255));
+        a.drawPolygon(x2,y2,7);
+        a.fillPolygon(x2,y2,7);
+
+        //Handle
+        a.setColor(new Color(230,255,255));
+        a.drawOval(814,305,20,10);
+        a.fillOval(814,305,20,10);
+
+        //Dog
+        int[] headx = {175,188,196,210,213,212,209,203,192,188,177,169,169,171,174,170,179};
+        int[] heady = {323,323,334,334,347,357,366,376,376,373,370,366,360,353,342,331,320};
+        a.setColor(new Color(191,128,64));
+        a.drawPolygon(headx, heady, 17);
+        a.fillPolygon(headx, heady, 17);
+        //Ear
+        int[] earx = {169,171,174,172,168,161,152,140,134,139,136,143,142,149,162};
+        int[] eary = {325,331,342,353,360,367,371,368,357,354,343,343,334,335,325};
+        a.setColor(new Color(77, 51, 25));
+        a.drawPolygon(earx,eary,15);
+        a.fillPolygon(earx,eary,15);
+
+
     }
     public static void main(String[] args) {
         
